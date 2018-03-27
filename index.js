7a08be4d2f89a7f84bb040a93d041343fc8cae5d
Jacques Bellisario <gypsyoak@yahoo.com>
2018-03-27 06:33:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+var musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
+var instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
+
+function theBeatlesPlay(musicians, instruments) {
+  const beatlesInfo = [];
+  for (let i = 0, l = musicians.length; i < l; i++) {
+  beatlesInfo.push(`${musicians[i]} plays ${instruments[i]}`)
+} return beatlesInfo;
+}
+
+function johnLennonFacts(facts) {
+  const facts = [];
+  let i = 0; 
+  while(i < facts.length) {
+  console.log(`${facts[i]}!!!`)
+  i++
+} return facts;
+}
