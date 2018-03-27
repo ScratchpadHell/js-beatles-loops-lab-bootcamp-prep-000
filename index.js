79c7fd25d2d4944c4b85a017282a02e61cf8d829
Jacques Bellisario <gypsyoak@yahoo.com>
2018-03-27 06:34:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(facts) {
-  const facts = [];
+  const loudFacts = [];
   let i = 0; 
   while(i < facts.length) {
   console.log(`${facts[i]}!!!`)
