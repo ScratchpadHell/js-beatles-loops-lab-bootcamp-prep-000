a32b82a41d37dd5c24154506a7fde7d61dea9e69
Jacques Bellisario <gypsyoak@yahoo.com>
2018-03-27 06:35:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(facts) {
-  const loudFacts = [];
+  const loudFacts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+  ];
   let i = 0; 
   while(i < facts.length) {
   console.log(`${facts[i]}!!!`)
